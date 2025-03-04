bf9be85678550954d243cbaf987d8cc6f689b5ad
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-03-04 12:04:29 +0530

    started

---
+++ Ai_Project_1/backend/src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
+
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+module.exports = app;
